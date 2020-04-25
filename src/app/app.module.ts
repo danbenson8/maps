426eb77c4d28be7e26c702af09d9c5add5aa439d
danbenson8 <danbenson8@gmail.com>
2020-04-25 09:32:09 +0100

    add state and map components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { StateComponent } from './state/state.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        HttpClientModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      StateComponent,
+      MapComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
