507c34cff85d260f68b84e0b77488f61deec9f2d
danbenson8 <danbenson8@gmail.com>
2020-04-25 09:31:28 +0100

    add method to get state paths.

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -11,4 +11,13 @@ import { catchError, retry } from 'rxjs/operators'
 export class MapService {
 
     constructor(private http: HttpClient) { }
+
+    dbUrl: string = 'http://localhost:3000/';
+    stateUrl: string;
+
+    getStatePaths(state: string) {
+        this.stateUrl = this.dbUrl + state;
+        return this.http.get(this.stateUrl);
+    }
+
 }
