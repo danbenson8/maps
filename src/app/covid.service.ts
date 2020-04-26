a6d8f9f5fb4ca6fe6818182cea6945421649cfbf
danbenson8 <danbenson8@gmail.com>
2020-04-26 22:47:33 +0100

    refactor states map to fit the api better (id is key rather than name is key).

--- src/app/covid.service.ts
+++ src/app/covid.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CovidService {
 
-constructor() { }
+    requestOptions: object = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    baseUrl: string = 'https://covidtracking.com/api/';
+    currentUrl: string;
+    historicalUrl: string;
+
+
+    constructor(private http: HttpClient) { }
+
+    getHistorical() {
+        this.historicalUrl = this.baseUrl + 'states/daily'
+        return this.http.get(this.historicalUrl, this.requestOptions);
+    }
+
+    getCurrent() {
+        this.currentUrl = this.baseUrl + 'states'
+        return this.http.get(this.currentUrl, this.requestOptions);
+    }
 
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit, HostListener, Input } from '@angular/core';
 import { MapService } from '../map.service';
+import { CovidService } from '../covid.service';
 
+class Data {
+    date: string;
+    positive: number;
+    negative: number;
+    recovered: number;
+    death: number;
+}
 class State {
-    id: string;
+    name: string;
     selected: boolean;
-    constructor(id: string) {
-        this.id = id;
+    data: Data[];
+    constructor(name: string) {
+        this.name = name;
         this.selected = false;
     }
     [Symbol.iterator]() {
@@ -19,22 +28,23 @@ class State {
 })
 export class MapComponent implements OnInit {
 
-    newStates = new Map();
+    states = new Map();
 
-    constructor(private mapService: MapService) { }
+    constructor(private mapService: MapService, private covidService: CovidService) { }
 
     ngOnInit() {
         // TODO error handling
         this.mapService.getAllStates()
             .subscribe(data => {
                 Object.keys(data).forEach(el => {
-                    this.newStates.set(el, new State(data[el][0]));
+                    this.states.set(data[el][0], new State(el));
                 });
             });
     }
 
     select(event: MouseEvent) {
         let state = event.target['title'];
-        if (this.newStates.has(state)) { this.newStates.get(state).selected = !this.newStates.get(state).selected; }
+        if (this.states.has(state)) { this.states.get(state).selected = !this.states.get(state).selected; }
+        console.log(this.states)
     }
 }
